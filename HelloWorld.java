242aba275da09a416fbad022512cafd6188c779e
mutaz <matzsl.955@gmail.com>
2026-09-14 13:50:37 +0300

    new vir add 2 num

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        greet("SE 4111 student");
-    }
-
-    public static void greet(String name) {
-        System.out.println("Welcome, " + name + "! Enjoying Lab 2 on Git?");
+       
+int x =1;
+int y=2;
+System.out.println((x+y));
+ 
     }
 }
