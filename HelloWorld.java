05e56fa47206e5d114a4742651049071ecc6caec
mutaz <matzsl.955@gmail.com>
2026-09-14 13:35:08 +0300

    Add greeting feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        greet("SE 4111 student");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Welcome, " + name + "! Enjoying Lab 2 on Git?");
     }
 }
